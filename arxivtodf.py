e1554db3ab09e0e44c5580e7af61cf87d71ec90c
RasmusBC59 <rbcmail@gmail.com>
2021-08-02 11:07:55 +0200

    refactored py3

---
+++ arxivtodf.py
@@ -0,0 +1,129 @@
+
+import urllib.request as libreq
+import feedparser
+import re
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def df_from_query(query: str, start: int = 0, max_results: int = 5):
+    entrys = get_arxiv_feed_entries_list(query, start, max_results)
+    df = df_from_arxiv_feed(entrys)
+    return df
+
+
+def get_arxiv_feed_entries_list(query: str, start: int = 0, max_results: int = 5):
+    base_url = 'http://export.arxiv.org/api/query?'
+    query_url = f'search_query={query}&start={start}&max_results={max_results}'
+
+    with libreq.urlopen(base_url+query_url) as url:
+        response = url.read()
+    feed = feedparser.parse(response)
+    return feed['entries']
+
+
+def df_from_arxiv_feed(entrys):
+    column_list = ['arxiv_id', 'idnr', 'Ver', 'updated', 'Year', 'journal_ref', 'DOI', 'title', 'summary', 'authors', 'ref_link', 'pdf_link', 'arxiv_abstract']
+    my_list = [entry_list(ent) for ent in entrys]
+    df = pd.DataFrame(my_list, columns=column_list)
+    df['title'] = df.apply(lambda x: update_tile(x.DOI, x.arxiv_id, x.title), axis=1)
+    return df
+
+
+def update_tile(doi: str, arxiv_id: str, title: str):
+    if doi is not None and arxiv_id is not None:
+        return get_title_from_crossref(doi)
+    else:
+        return title
+
+
+def get_title_from_crossref(doi: str):
+    basehref_crossref = f'http://doi.crossref.org/servlet/query?usr=guenevere.p@gmail.com&format=unixref&qdata={doi}'
+    with libreq.urlopen(basehref_crossref) as url:
+        response = url.read()
+    root = ET.fromstring(response)
+    for title in root.iter('title'):
+        title_str = title.text
+    return title_str
+
+
+def entry_list(entry):
+    idtuple = get_id_and_version(entry)
+    year = get_year(entry)
+    ref_link = get_link(entry, 'doi')
+    pdf_link = get_pdf_link(entry)
+    arxiv_abstract = get_arxiv_abstract(entry)
+    entrylist = [idtuple[0], idtuple[1], idtuple[2],
+                 entry.updated,
+                 year,
+                 has_key_else_none(entry, 'arxiv_journal_ref'),
+                 has_key_else_none(entry, 'arxiv_doi'),
+                 entry.title,
+                 entry.summary,
+                 entry.authors,
+                 ref_link,
+                 pdf_link,
+                 arxiv_abstract]
+    return entrylist
+
+
+def get_id_and_version(entry):
+    arxiv_id = entry['id']
+    last_slash_index = arxiv_id.rindex('/')
+    idv = arxiv_id[last_slash_index+1:]
+    v_index = idv.rindex('v')
+    idnr = idv[:v_index]
+    version = idv[v_index+1:]
+    return (arxiv_id, idnr, version)
+
+
+def get_year(entry):
+    year_arxiv_updated = entry.updated_parsed.tm_year
+    if 'arxiv_journal_ref' in entry:
+        ref = entry.arxiv_journal_ref
+        year_re = re.search("\(20[0-5][0-9]\)", ref)
+        if year_re is not None:
+            year = year_re.group()[1:-1]
+        else:
+            year = year_arxiv_updated  #'1950'
+    else:
+        year = year_arxiv_updated
+    return str(year)
+
+
+def get_link(entry, link_title):
+    for link in entry.links:
+        if link.rel != 'alternate':
+            if link.title == link_title:
+                link = link.href
+                return link
+    return None
+
+
+def get_arxiv_abstract(entry):
+    for link in entry.links:
+        if link.rel == 'alternate':
+            link = link.href
+            return link
+
+
+def get_pdf_link(entry):
+    basehref_pdfs = 'http://qdev-data.nbi.ku.dk/pdfs'
+    pdf_link = basehref_pdfs+'/'+entry.id.split('/abs/')[-1]+'.pdf'
+    try:
+        tryopen = libreq.urlopen(pdf_link)
+        return pdf_link
+    except:
+        try:
+            pdf_link = get_link(entry, 'pdf')
+            tryopen = libreq.urlopen(pdf_link)
+            return pdf_link
+        except:
+            return None
+
+
+def has_key_else_none(entry, key):
+    if key in entry:
+        return entry[key]
+    else:
+        return None
---
+++ dftohtml.py
@@ -0,0 +1,149 @@
+import re
+from unidecode import unidecode
+
+
+def df_to_html_file(homepageid, fullname, df, prof_names, file_path):
+    stringhtml = make_html_string(homepageid, fullname, df, prof_names)    
+    html_string_to_file(file_path, stringhtml)
+
+
+def html_string_to_file(file_path, html_string):
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(html_string)
+
+
+def make_html_string(homepageid, fullname, df, prof_names):
+    stringhtml = output_html_string_top(homepageid, fullname)
+    stringhtml += entrys_by_years_to_html(df, prof_names)
+    stringhtml += '</body></html>'
+    return stringhtml
+
+
+def output_html_string_top(homepageid, fullname):
+    output_html_string_top = '<html><head>\n'
+    output_html_string_top += '<meta http-equiv="content-type" content="text/html;charset=utf-8" />\n'
+    output_html_string_top += '<link href="http://qdev.nbi.ku.dk/css/style.css" rel="stylesheet" type="text/css" media="screen, print" />\n'
+    output_html_string_top += '<link rel="stylesheet" type="text/css" href="http://qdev-data.nbi.ku.dk/publications/simpletree.css" />'
+    output_html_string_top += '</head><body>\n'
+    output_html_string_top += '<script type="text/javascript" src="http://qdev-data.nbi.ku.dk/publications/simpletreemenu.js"></script>\n'
+    output_html_string_top += '<script type="text/javascript"\n'
+    output_html_string_top += '  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">\n'
+    output_html_string_top += '</script>\n'
+    output_html_string_top += '<script type="text/x-mathjax-config">\n'
+    output_html_string_top += 'MathJax.Hub.Config({\n'
+    output_html_string_top += '  tex2jax: {inlineMath: [["$","$"], ["{","}"]]}\n'
+    output_html_string_top += '});\n'
+    output_html_string_top += '</script>\n'
+    # If NOT all profs, write his name with a link to his page
+    if homepageid != '':
+        output_html_string_top += f'<h2>Publications by <a href="http://www.nbi.ku.dk/english/staff/?id={homepageid}&vis=medarbejder">{fullname}</a></h2>'
+    # If it IS all profs, write name (with no link), which is all QDev staff
+    else:
+        output_html_string_top += f'<h2>Publications by {fullname}</h2>'
+#    output_html_string_top += '<p><img src="%s" style="width:100px;">' %qdev_logo
+    output_html_string_top += '</p>\n'
+    return output_html_string_top
+
+
+def entrys_by_years_to_html(df, prof_names):
+    years_list = list(set(df.Year.to_list()))
+    years_list.sort(reverse=True)
+    output_html_string = ''
+    for year in years_list:
+        df_year = df.loc[df['Year'] == year]
+        output_html_string += entrys_year_to_html(year, df_year, prof_names)
+
+    return output_html_string
+
+
+def entrys_year_to_html(year, df, prof_names):
+    output_html_string = f'<ul id="{year}" class="treeview">\n'
+    output_html_string += f'<li><a href="javascript:void(0)" onClick="return false;" style="font-size: 12px; font-weight:bold; line-height: 3em">{year}</a>\n<ul rel="open">\n'
+
+    output_html_string += entrys_to_html(df, prof_names)
+
+    output_html_string += '</ul>\n</li></ul>\n'
+    output_html_string += '<script type="text/javascript">\n'
+    output_html_string += '//ddtreemenu.createTree(treeid, enablepersist, opt_persist_in_days (default is 1))\n'
+    output_html_string += f'ddtreemenu.createTree("{year}", false)\n'
+    output_html_string += "</script>\n"
+    return output_html_string
+
+
+def entrys_to_html(df, prof_names):
+    stringhtml = ''
+    df_dict = df.to_dict('records')
+    for i in range(df.shape[0]):
+        entry = df_dict[i]
+        stringhtml += output_html_string_entry(entry, prof_names)
+    return stringhtml
+
+
+def output_html_string_entry(entry, prof_names):
+    output_html_string = '<li>\n'
+    output_html_string += '<b>\n'
+    output_html_string += entry['title']
+    output_html_string += '</b> - \n'
+    output_html_string += '<a href="javascript:void(0)" onClick="return false;" rel="closed">Abstract</a>\n'
+    output_html_string += '<ul rel="closed"><li>\n'
+    output_html_string += entry['summary']
+    output_html_string += '</li></ul></li><li>'
+    output_html_string += '<table><tr>\n'
+    output_html_string += '<td>\n'
+    output_html_string += '</td></tr><tr><td>\n'
+
+    output_html_string += author_links_to_html(entry['authors'], prof_names)
+
+    output_html_string += journal_link_to_html(entry)
+
+    if entry['DOI']:
+        output_html_string += '<tr><td>DOI: '+entry['DOI']
+        output_html_string += '</td></tr><tr><td>\n'
+
+    if not(entry['journal_ref']):
+        if entry['arxiv_abstract']:
+            idv = entry['arxiv_id'].split('/abs/')[-1]
+            output_html_string += '<a href='+entry['arxiv_abstract']+' target="_blank" title="Abstract">'+idv+'</a> '
+        if entry['pdf_link']:
+            output_html_string += '[<a href="{}" title="Download PDF">pdf</a>]'.format(entry['pdf_link'])
+
+        output_html_string += '</td></tr>'
+
+    output_html_string += '</table><hr>\n'
+    output_html_string += '</li>\n'
+
+    return output_html_string
+
+
+def author_links_to_html(authors, prof_names):
+    output_html_string = ''
+    for i, author in enumerate(authors):
+        linkaddr = '<a href="http://arxiv.org/find/cond-mat/1/au:+' + re.sub('[^A-Za-z0-9-_]+', '', re.sub('-', '_', unidecode(author.name[author.name.rfind(' ')+1:]))) + '_' + unidecode(author.name[0]) + '/0/1/0/all/0/1" target="_blank">' + author.name + '</a>'
+        for prof_name in author.name.lower().split():
+            if prof_name in prof_names:
+                linkaddr = '<a href="http://qdev.nbi.ku.dk/publications/' + prof_name + '">' + author.name + '</a>'
+        if i != 0:
+            output_html_string += ', '
+        output_html_string += linkaddr
+    output_html_string += '</td></tr><tr><td>\n'
+    return output_html_string
+
+
+def journal_link_to_html(entry):
+    journal_ref = ''
+    if entry['journal_ref'] and entry['ref_link']:
+        journal_ref += '<tr><td>Journal reference: '
+        journal_ref += '<a href="javascript:void(0)" onclick="window.open('
+        journal_ref += "'"
+        journal_ref += entry['ref_link']
+        journal_ref += "', 'newwindow', 'width=800, height=800'); return false;"
+        journal_ref += '">'
+        journal_ref += entry['journal_ref']
+        journal_ref += '</a>\n'
+
+    if entry['pdf_link']:
+        journal_ref += ' [<a href='+entry['pdf_link']+' title="Download PDF">pdf</a>]'
+    #else:
+        #output_html_string += '<font color=#fff> [ %s ]</font>' %entry.id.split('/abs/')[-1]
+    journal_ref += '</td></tr>\n'
+    return journal_ref
